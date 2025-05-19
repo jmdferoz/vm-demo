e06dad8827a380fd7b5b9d1a0d38feca5f7066ec
jmdferoz <55978134+jmdferoz@users.noreply.github.com>
2025-05-19 19:16:59 +0530

    Update main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -26,6 +26,8 @@ resource "azurerm_windows_virtual_machine" "vm" {
   location            = var.location
   resource_group_name = var.resource_group_name
   size                = var.vm_size
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
   
   network_interface_ids = [
     azurerm_network_interface.nic.id
