ba89fce9f9fd8e565badaeac7a16a06250068dde
jmdferoz <55978134+jmdferoz@users.noreply.github.com>
2025-05-19 19:15:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,4 +6,6 @@ module "windows_vm" {
   vm_size             = var.vm_size
   subnet_name         = var.subnet_name
   vnet_name           = var.vnet_name
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
 }
